6e1d30a806e211ab30bb009f64d4a8c5cd61dcea
Nimesh <chaudharynimesh3456@gmail.com>
2023-10-05 11:20:46 -0400

    Assets Directory created and placed placeholder logo
    We can add our graphical assets to the assets folder. For now i added  placeholder logo but we have to change it.

--- site/src/components/Header/index.js
+++ site/src/components/Header/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './styles.scss';
+import Logo from './../../assets/logo.png'
 
 const Header = props => {
     return (
         <header className='header'>
-
+            <div className='wrap'>
+                <div className='logo'>
+                    <img src={Logo} alt='Play Universe logo'/>
+                </div>
+            </div>
         </header>
     )
 };
